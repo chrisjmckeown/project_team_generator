c9d6ee8fda9e5aa2c202f1803434d5da6b4b4e47
Chris Mckeown <chris.j.mckeown@hotmail.com>
2020-10-11 08:48:11 +1100

    basic app working

--- app.js
+++ app.js
@@ -4,12 +4,126 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require('util');
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeeTypes = ["Manager", "Engineer", "Intern", "Team complete (exit)"];
+
+function promptRepo() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            message: "Please select an employee type:",
+            name: "result",
+            choices: [
+                ...employeeTypes
+            ]
+        }
+    ]);
+}
+
+function prompEmployeeInfo() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Please enter user name:",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter user id:",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Please enter user email:",
+            name: "email"
+        }
+    ]);
+}
+
+function promptManagerInfo() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Please enter user office number:",
+            name: "officeNumber"
+        }
+    ]);
+}
+
+function promptEngineerInfo() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Please enter user GitHub url:",
+            name: "github"
+        }
+    ]);
+}
+
+function promptInternInfo() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Please enter user school:",
+            name: "school"
+        }
+    ]);
+}
+
+function folderExists(outputPath) {
+    try {
+        return fs.lstatSync(outputPath).isDirectory();
+    } catch (_) {
+        return false;
+    }
+}
+
+async function promptTeamInfo() {
+    let builtingTeam = true;
+    const employeeList = [];
+    while (builtingTeam) {
+        const { result } = await promptRepo();
+        switch (result) {
+            case "Team complete (exit)":
+                builtingTeam = false;
+                break;
+            case "Manager":
+                var { name, id, email } = await prompEmployeeInfo()
+                let { officeNumber } = await promptManagerInfo();
+                const manager = new Manager(name, id, email, officeNumber);
+                employeeList.push(manager);
+                break;
+            case "Engineer":
+                var { name, id, email } = await prompEmployeeInfo()
+                let { github } = await promptEngineerInfo();
+                const engineer = new Engineer(name, id, email, github);
+                employeeList.push(engineer);
+                break;
+            case "Intern":
+                var { name, id, email } = await prompEmployeeInfo()
+                let { school } = await promptInternInfo();
+                const intern = new Intern(name, id, email, school);
+                employeeList.push(intern);
+                break;
+            default:
+            // code block
+        }
+    }
+    const html = render(employeeList);
+  //  console.log(html);
+    
+    await writeFileAsync(outputPath, html, "utf8");
+    console.log(`Successfully generated the team file. \nSee here: ${outputPath}`);
+}
+
+promptTeamInfo();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
